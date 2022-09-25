fd7227dec06e3fdf0bd418e97c7d31e59010f30f
Thomashallberg <thomas.hallberg91@outlook.com>
2022-09-25 22:13:08 +0200

    added read accounts from file

--- bankomat1.py
+++ bankomat1.py
@@ -1,13 +1,26 @@
+from contextlib import nullcontext
 import sys
 from datetime import date
+from tkinter import N
 
 
 from termcolor import colored, cprint
+transaktioner = {}
+def ReadFromFile():
+    AllaKonton = {}
 
-allAccounts = {}
-transaktioner = {
+    with open("saldo.txt", "r") as filen:
+        for raden in filen:
+            kontonamn = raden.split()
+            print (f"{kontonamn[0]}")
+            print (f"{kontonamn[1]}")
+            AllaKonton[kontonamn[0]] = int(kontonamn[1])
+            transaktioner [kontonamn[0]] = []
+            #print(AllaKonton)
+    return AllaKonton
+allAccounts = ReadFromFile()
     #"namn": ["transaktion + today"]
-}
+
 
 while True:
     print("1. Skapa konto")
@@ -40,22 +53,23 @@ while True:
                 if action == "1":
                     today = date.today()
                     belopp = int(input("ange belopp att sätta in"))
-                    allAccounts[namn] = allAccounts[namn] + belopp
-                    transaktioner[namn].append(f"{namn} satte in {str(belopp)} kronor: {str(today)}")
+                    allAccounts[NuvarandeKonto] = allAccounts[NuvarandeKonto] + belopp
+                    transaktioner[NuvarandeKonto].append(f"{NuvarandeKonto} satte in {str(belopp)} kronor: {str(today)}")
 
 
                 elif action == "2":
                     today = date.today()
                     belopp = int(input("ange belopp att ta ut"))
-                    if allAccounts[namn] - belopp < 0:
+                    if allAccounts[NuvarandeKonto] - belopp < 0:
                         print("Du har för lite pengar")
-                    allAccounts[namn] = allAccounts[namn] - belopp
-                    transaktioner[namn].append(f"{namn} tog ut {str(belopp)} kronor: {str(today)}")
+                    allAccounts[NuvarandeKonto] = allAccounts[NuvarandeKonto] - belopp
+                    transaktioner[NuvarandeKonto].append(f"{NuvarandeKonto} tog ut {str(belopp)} kronor: {str(today)}")
                   
                 if action == "3":
-                    print(f"Du har {allAccounts[namn]} kronor på ditt konto")
+                    print(f"Du har {allAccounts[NuvarandeKonto]} kronor på ditt konto")
                 elif action == "4":
-                    print(transaktioner[namn])
+                    print(transaktioner[NuvarandeKonto])
+                    
                 if action == "5":
                     break
     elif action == "3":
